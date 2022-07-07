b2579a93bf714ce5c30fb0423bcf9dd8bb419ced
tutorial6 <manapure.vaibhav542@gmail.com>
2022-07-07 23:04:33 +0530

    Add UC1 solution

---
+++ DataStructure/DataStructure/BinarySearchTree.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    public class BinarySearchTree
+    {
+        public Node Root { get; set; }
+
+        public bool Add(int value)
+        {
+            Node before = null, after = Root;
+            while (after != null)
+            {
+                before = after;
+                if (value < after.Data)
+                    after = after.LeftNode;
+                else if (value > after.Data)
+                    after = after.RightNode;
+                else
+                {
+                    return false;
+                }
+            }
+
+            Node newNode = new Node();
+            newNode.Data = value;
+
+            if (this.Root == null)
+            {
+                this.Root = newNode;
+            }
+            else
+            {
+                if (value < before.Data)
+                    before.LeftNode = newNode;
+                else
+                    before.RightNode = newNode;
+            }
+            return true;
+        }
+
+        public void TravarsePreorder(Node Parent)
+        {
+            if (Parent != null)
+            {
+                Console.WriteLine(Parent.Data + " ");
+                TravarsePreorder(Parent.LeftNode);
+                TravarsePreorder(Parent.RightNode);
+            }
+        }
+
+
+        public void TraverseInOrder(Node parent)
+        {
+            if (parent != null)
+            {
+                TraverseInOrder(parent.LeftNode);
+                Console.WriteLine(parent.Data + " ");
+                TraverseInOrder(parent.RightNode);
+            }
+        }
+
+        public void TraversePostOrder(Node parent)
+        {
+            if (parent != null)
+            {
+                TraversePostOrder(parent.LeftNode);
+                TraversePostOrder(parent.RightNode);
+                Console.WriteLine(parent.Data + " ");
+            }
+        }
+    }
+}
---
+++ DataStructure/DataStructure/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySearchTree bst = new BinarySearchTree();
+            bst.Add(56);
+            bst.Add(30);
+            bst.Add(70);
+
+            Console.WriteLine("Preorder Traversal- ");
+            bst.TravarsePreorder(bst.Root);
+            Console.WriteLine("Inorder Traversal- ");
+            bst.TraverseInOrder(bst.Root);
+            Console.WriteLine("Postorder Traversal- ");
+            bst.TraversePostOrder(bst.Root);
+        }
+    }
+}
